752148ed62fbff4b5890ffd33d18837d30362c1f
Joe Antony <joeantony4u@gmail.com>
2010-11-14 00:54:06 +0530

    Signed-off-by: Joe Antony <joeantony4u@gmail.com>

--- src/org/np/stoman/dao/support/HibernateSupport.java
+++ src/org/np/stoman/dao/support/HibernateSupport.java
@@ -1,33 +1,32 @@
 package org.np.stoman.dao.support;
 
-import java.util.Collection;
-import java.util.LinkedList;
 import java.util.List;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.criterion.Criterion;
-import org.hibernate.criterion.Restrictions;
+import org.hibernate.criterion.Order;
 
 public class HibernateSupport {
-	
+
 	private static HibernateSupport hs;
 	private final ThreadLocal<Session> session;
-	
+
 	private HibernateSupport() {
 		session = new ThreadLocal<Session>();
 	}
-	
+
 	public synchronized static HibernateSupport getInstance() {
-		if(hs == null)
+		if (hs == null)
 			hs = new HibernateSupport();
 		return hs;
 	}
-	
+
 	public <T> void save(T t) {
 		session.get().save(t);
 	}
-	
-	public <T> T get(Class<T> t, int id){
+
+	public <T> T get(Class<T> t, int id) {
 		return t.cast(session.get().load(t, id));
 	}
 
@@ -35,24 +34,34 @@ public class HibernateSupport {
 	public <T> List<T> get(Class<T> t) {
 		return session.get().createCriteria(t).list();
 	}
-	
-	public List<Criterion> restrict(Restrict restrict, Object[]... values) {
-		List<Criterion> criterions = new LinkedList<Criterion>();
-		for(Object[] value : values) {
-			switch (restrict) {
-			case IN:
-				criterions.add(Restrictions.in((String)value[0], (Collection)value[1]));
-				break;
 
-			case NOTIN:
-				Restrictions.not(Restrictions.in((String)value[0], (Collection)value[1]));
-				break;
-			}
-			
-		}
-		return criterions;
+	@SuppressWarnings("unchecked")
+	public <T> List<T> get(Class<T> t, List<Criterion>... criterions) {
+		Criteria criteria = session.get().createCriteria(t);
+		addRestrictions(criteria, criterions);
+		return criteria.list();
 	}
-	
+
+	@SuppressWarnings("unchecked")
+	public <T> List<T> get(Class<T> t, List<Order> orders,
+			List<Criterion>... criterions) {
+		Criteria criteria = session.get().createCriteria(t);
+		addRestrictions(criteria, criterions);
+		addOrders(criteria, orders);
+		return criteria.list();
+	}
+
+	private void addRestrictions(Criteria criteria, List<Criterion>[] criterions) {
+		for (List<Criterion> lcriterion : criterions)
+			for (Criterion criterion : lcriterion)
+				criteria.add(criterion);
+	}
+
+	private void addOrders(Criteria criteria, List<Order> orders) {
+		for (Order order : orders)
+			criteria.addOrder(order);
+	}
+
 	public void setSession(Session session) {
 		this.session.set(session);
 	}
--- src/test/Test.java
+++ src/test/Test.java
@@ -1,5 +1,13 @@
 package test;
 
+import static org.np.stoman.dao.support.FieldConstants.ADDRESS;
+import static org.np.stoman.dao.support.FieldConstants.NAME;
+import static org.np.stoman.dao.support.Order.DESC;
+import static org.np.stoman.dao.support.Restrict.NOTEQ;
+import static org.np.stoman.dao.support.Restrict.NULL;
+
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.np.stoman.dao.conf.HibernateUtil;
@@ -11,21 +19,34 @@ public class Test {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		Session s=HibernateUtil.openSession();
-		Transaction tr=s.beginTransaction();
-		Vendors v=new Vendors();
+	public static void main1(String[] args) {
+		Session s = HibernateUtil.openSession();
+		Transaction tr = s.beginTransaction();
+		Vendors v = new Vendors();
 		v.setName("V2");
 		HibernateSupport.getInstance().setSession(s);
 		HibernateSupport.getInstance().save(v);
 		tr.commit();
 		s.close();
 		System.out.println("After save");
-		Session s1=HibernateUtil.openSession();
+		Session s1 = HibernateUtil.openSession();
 		HibernateSupport.getInstance().setSession(s1);
 		v = HibernateSupport.getInstance().get(v.getClass()).get(0);
-		System.out.println("Id"+v.getVendorId());
+		System.out.println("Id" + v.getVendorId());
 		s1.close();
 	}
 
+	@SuppressWarnings("unchecked")
+	public static void main(String[] args) {
+		Session s = HibernateUtil.openSession();
+		HibernateSupport.getInstance().setSession(s);
+		long st = System.currentTimeMillis();
+		List<Vendors> vendors = HibernateSupport.getInstance().get(
+				Vendors.class, DESC.order(new String[] { NAME }),
+				NULL.restrict(new Object[] { ADDRESS }),
+				NOTEQ.restrict(new Object[] { NAME, "V1" }));
+		System.out.println(System.currentTimeMillis() - st);
+		for (Vendors vendor : vendors)
+			System.out.println(vendor.getName());
+	}
 }
